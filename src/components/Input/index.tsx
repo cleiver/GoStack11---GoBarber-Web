086853cb6f2b291dfbf51fe1d8381fde79a3ef3f
cleiver <cleiver@gmail.com>
2020-04-23 16:20:35 -0300

    🚸 Small usability improvement

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,10 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 import { Container } from './styles';
@@ -10,9 +16,15 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...props }) => {
-  const inputRef = useRef(null);
+  // unform stuff
+  const inputRef = useRef<HTMLInputElement>(null);
   const { fieldName, defaultValue, error, registerField } = useField(name);
 
+  // we'll use this to change the input color
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  // registering this input on unform
   useEffect(() => {
     registerField({
       name: fieldName,
@@ -20,10 +32,30 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...props }) => {
       path: 'value',
     });
   }, [fieldName, registerField]);
+
+  // useCallback "memorize" our function so javascript do not recreate
+  // it on every render
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  // show the icon if one is provided
+  // onfocus, change the colors of the input and the icon
+  // onblur, undo onfocus
   return (
-    <Container>
+    <Container isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
-      <input ref={inputRef} defaultValue={defaultValue} {...props} />
+      <input
+        ref={inputRef}
+        defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        {...props}
+      />
     </Container>
   );
 };
